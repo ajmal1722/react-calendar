dcf8095b1636d4901033192ba0a68766e67e6366
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-05-04 21:00:01 +0530

    input value added in uI

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,12 +1,23 @@
 import  { useState } from 'react';
 import ReactCalendar from 'react-calendar';
+import { useSelector, useDispatch } from 'react-redux';
+import { addToEvents } from '../Redux/events/eventSlice'
 
 const Calendar = () => {
     const [value, onChange] = useState(new Date());
     const [showModal, setShowModal] = useState(false);
+    const [eventText, setEventText] = useState('');
+    const events = useSelector(state => state.events.value);
+    const dispatch = useDispatch()
 
     const enableModal = () => {
         setShowModal(prevState => !prevState)
+    }
+
+    const handleEvents = () => {
+        dispatch(addToEvents(eventText))
+        enableModal()
+        setEventText(''); 
     }
 
     return (
@@ -19,20 +30,22 @@ const Calendar = () => {
                     <div className="flex items-center justify-center h-screen">
                         <div className="bg-gray-400 rounded-xl">
                             <div className="px-6">
-                                <input type="text" className='py-2 px-3 my-8 rounded-md' placeholder='Event...'/>
+                                <input type="text" className='py-2 px-3 my-8 rounded-md' placeholder='Event...'
+                                value={eventText}
+                                onChange={(e) => setEventText(e.target.value)}
+                                />
                                 <button className="ml-12" onClick={enableModal}>
-                                    x
+                                    close
                                 </button>
                             </div>
-                            <button className='mb-6 bg-green-600 w-24'>
-                                save
+                            <button className='mb-6 bg-green-600' onClick={handleEvents}>
+                                Add Event
                             </button>
                         </div>
                     </div>
                 </div>
             )}
         </div>
-        
     )
 }
 
