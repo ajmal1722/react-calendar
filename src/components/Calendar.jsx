2f4ec4ae4fa65a6dbedb196a6ccbdd6cdc7152ad
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-05-04 18:14:01 +0530

    enabled modal

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -12,19 +12,19 @@ const Calendar = () => {
     return (
         <div className='bg-indigo-200 p-2'>
             <div className="sm:w-96">
-                <ReactCalendar onChange={onChange} value={value} />
+                <ReactCalendar onChange={onChange} value={value} onClickDay={enableModal}/>
             </div>
             { showModal && (
                 <div className="fixed h-full w-screen backdrop-blur top-0 right-0">
                     <div className="flex items-center justify-center h-screen">
                         <div className="bg-gray-400 rounded-xl">
                             <div className="px-6">
-                                <input type="text" className='py-1 px-3 my-8 rounded-md' placeholder='Event...'/>
+                                <input type="text" className='py-2 px-3 my-8 rounded-md' placeholder='Event...'/>
                                 <button className="ml-12" onClick={enableModal}>
                                     x
                                 </button>
                             </div>
-                            <button className='mb-6 bg-green-600' >
+                            <button className='mb-6 bg-green-600 w-24'>
                                 save
                             </button>
                         </div>
